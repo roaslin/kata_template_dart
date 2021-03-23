20a30167865b1d5cd93705942dec573f0bd86e0f
Raul Mordillo Lluva <raul.mordillo@koahealth.com>
2021-03-23 15:12:19 +0100

    test skeleton

---
+++ test/test_sample.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+import '../src/roman.dart';
+
+void main() {
+  group('My thing should', () {
+    test('Do something', () {
+      // var result = rocket.explode();
+
+      expect(1, equals(1));
+    });
+  });
+});
+}
